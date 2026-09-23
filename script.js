4fd0f3d8f127d69d24e318e659f5fce11297405f
vivek bharamshetti <69708672+viveklb@users.noreply.github.com>
2026-09-23 11:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const count = {};
+
+  for (let char of str) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  for (let char of str) {
+    if (count[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+
+
+	
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
